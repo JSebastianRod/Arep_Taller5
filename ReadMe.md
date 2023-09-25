71ecbc110361af91f5173ef7febf5010e88ed8e5
JSebastianRod <sebastianrodriguez019@gmail.com>
2023-09-25 18:44:18 -0500

    Readme y codigo terminado

--- ReadMe.md
+++ ReadMe.md
@@ -1,11 +1,150 @@
+# Bonus Parcial
+
+En este repositorio se encuentra el bonus del parcial de AREP, el cual calcula: Seno, Coseno, Palindromo y la magnitud de un vector real.
+
+## Para empezar
+
+Descargue y copie el repositorio en su maquina local.
+
+### Prerrequisitos
+
+- Java
+- Maven
+
+### Ejecución
+
+Primero, compile el proyecto con el siguiente comando:
+
+```
+mvn clean install
+```
+
+Después, ejecute el siguiente comando para correr el programa:
+
+```
 java -cp "target/classes;target/dependency/*" edu.escuelaing.arep.app.SparkWebServer
+```
+
+Debe ingresar a la siguiente direccion:
+
+```
+http://localhost:4567/index
+```
+
+Estando ahi la pagina se vera asi:
+<br>
 
 ![](./src/images/1.png)
-![](./src/images/2.png)
-![](./src/images/3.png)
-![](./src/images/4.png)
-![](./src/images/5.png)
-![](./src/images/6.png)
 
-link docker 
+
+
+### Funcionamiento y Código
+
+### Función Seno
+
+El siguiente código calcula el seno de un numero ingresado por el usuario.
+<br>
+
+![](./src/images/sen.png)
+<br>
+Esto se ve en pantalla asi:
+<br>
+
+![](./src/images/1.png)
+
+
+Si hacemos el calculo por la url se ve asi:
+<br>
+
+![](./src/images/2.png)
+
+Y desde la aplicación asi:
+<br>
+
+![](./src/images/2.5.png)
+
+### Función Coseno
+El siguiente código calcula el Coseno de un numero ingresado por el usuario.
+<br>
+
+![](./src/images/9.png)
+<br>
+Esto se ve en pantalla asi:
+<br>
+
+![](./src/images/8.png)
+
+
+Si hacemos el calculo por la url se ve asi:
+<br>
+
+![](./src/images/11.png)
+
+Y desde la aplicación asi:
+<br>
+
+![](./src/images/12.png)
+
+### Función Palindromo
+El siguiente valida si una palabra es palindromo o no.
+<br>
+
+![](./src/images/pal.png)
+<br>
+Esto se ve en pantalla asi:
+<br>
+
+![](./src/images/pal1.png)
+
+
+Si hacemos la función desde la la url se ve asi:
+<br>
+
+Cuando es palindromo
+<br>
+![](./src/images/pl1.png)
+
+<br>
+Y cuando no
+
+![](./src/images/pl2.png)
+
+
+Y desde la aplicación asi:
+
+
+Cuando no es Palindromo
+<br>
+
+![](./src/images/rp1.png)
+
+<br>
+Cuando es palindromo
+<br>
+
+![](./src/images/rp2.png)
+
+### Función Vector
+El siguiente código calcula la magnitud de un vector real el cual recibe dos entradas.
+<br>
+
+![](./src/images/vec1.png)
+<br>
+Esto se ve en pantalla asi:
+<br>
+
+![](./src/images/vec2.png)
+
+
+Si hacemos el calculo por la url se ve asi:
+<br>
+
+![](./src/images/vec3.png)
+
+Y desde la aplicación asi:
+<br>
+
+![](./src/images/vec4.png)
+
+link docker
 https://hub.docker.com/r/sebastianrod/bonoparcial
--- src/main/java/edu/escuelaing/arep/app/SparkWebServer.java
+++ src/main/java/edu/escuelaing/arep/app/SparkWebServer.java
@@ -12,28 +12,34 @@ public class SparkWebServer {
         port(getPort());
         get("hello", (req, res) -> "Hello Docker!");
         System.out.println("Inicio");
-        get("index", (req, res) -> getIndexResponseSen() );
+        get("index", (req, res) -> getIndexResponseSen());
         get("/sen", (req, res) -> Math.sin((Double.parseDouble(req.queryParams("value")))));
         get("/cos", (req, res) -> Math.cos((Double.parseDouble(req.queryParams("value")))));
         get("/pal", (req, res) -> {
             String str = req.queryParams("value");
             String rstr = "";
             int strLength = str.length();
-            for (int i = (strLength - 1); i >=0; --i) {
+            for (int i = (strLength - 1); i >= 0; --i) {
                 rstr = rstr + str.charAt(i);
-              }
-          
-              if (str.toLowerCase().equals(rstr.toLowerCase())) {
+            }
+
+            if (str.toLowerCase().equals(rstr.toLowerCase())) {
                 return " Es palindromo.";
-              }
-              else {
+            } else {
                 return " No es palindromo.";
-              }
+            }
+        });
+        get("/vec", (req, res) -> {
+            double x = Double.parseDouble(req.queryParams("x"));
+            double y = Double.parseDouble(req.queryParams("y"));
+
+            double magnitud = Math.sqrt(x * x + y * y);
+
+            return "La magnitud del vector es: " + magnitud;
 
         });
     }
 
-    
     private static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
@@ -42,8 +48,7 @@ public class SparkWebServer {
     }
 
     public static String getIndexResponseSen() {
-        String response = 
-                "\r\n"
+        String response = "\r\n"
                 + "<!DOCTYPE html>\n"
                 + "<html\n>"
                 + "    <head\n>"
@@ -56,7 +61,7 @@ public class SparkWebServer {
                 + "        <h2>Indique la funcion</h2>\n"
                 + "        <form action=\"/hello\">\n"
                 + "            <label for=\"name\">Funtion Seno:</label><br>\n"
-                + "            <input type=\"text\" id=\"name\" name=\"name\" value=\" \"><br><br>\n"
+                + "            <input type=\"text\" id=\"name\" name=\"name\" value=\"\"><br><br>\n"
                 + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n"
                 + "        </form>\n"
                 + "        <div id=\"getrespmsg\"></div>\n"
@@ -72,21 +77,59 @@ public class SparkWebServer {
                 + "                xhttp.send();\n"
                 + "            }\n"
                 + "      </script>\n"
-                + "        <form action=\"/hello\">\n"
+                + "        <form action=\"/coseno\">\n"
                 + "            <label for=\"name\">Funtion Coseno:</label><br>\n"
-                + "            <input type=\"text\" id=\"name\" name=\"name\" value=\" \"><br><br>\n"
-                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n"
+                + "            <input type=\"text\" id=\"nameCos\" name=\"name\" value=\" \"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsgCos()\">\n"
                 + "        </form>\n"
-                + "        <div id=\"getrespmsg\"></div>\n"
+                + "        <div id=\"getrespmsgCos\"></div>\n"
                 + "      <script>\n"
-                + "            function loadGetMsg() {\n"
-                + "                let nameVar = document.getElementById(\"name\").value;\n"
+                + "            function loadGetMsgCos() {\n"
+                + "                let nameVar = document.getElementById(\"nameCos\").value;\n"
                 + "                const xhttp = new XMLHttpRequest();\n"
                 + "                xhttp.onload = function() {\n"
-                + "                    document.getElementById(\"getrespmsg\").innerHTML =\n"
+                + "                    document.getElementById(\"getrespmsgCos\").innerHTML =\n"
                 + "                    this.responseText;\n"
                 + "                } \n"
                 + "                xhttp.open(\"GET\", \"cos?value=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "      </script>\n"
+                + "        <form action=\"/palindromo\">\n"
+                + "            <label for=\"name\">Funcion Palindromo:</label><br>\n"
+                + "            <input type=\"text\" id=\"namePal\" name=\"name\" value=\" \"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsgPal()\">\n"
+                + "        </form>\n"
+                + "        <div id=\"getrespmsgPal\"></div>\n"
+                + "      <script>\n"
+                + "            function loadGetMsgPal() {\n"
+                + "                let nameVar = document.getElementById(\"namePal\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespmsgPal\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                } \n"
+                + "                xhttp.open(\"GET\", \"pal?value=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "      </script>\n"
+                + "        <form action=\"/vector\">\n"
+                + "            <label for=\"name\">Función Vector:</label><br>\n"
+                + "            <input type=\"text\" id=\"value1\" name=\"name\" value=\" \"><br><br>\n"
+                + "            <input type=\"text\" id=\"value2\" name=\"name\" value=\" \"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsgVec()\">\n"
+                + "        </form>\n"
+                + "        <div id=\"getrespmsgVec\"></div>\n"
+                + "      <script>\n"
+                + "            function loadGetMsgVec() {\n"
+                + "                let nameVar1 = document.getElementById(\"value1\").value;\n"
+                + "                let nameVar2 = document.getElementById(\"value2\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespmsgVec\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                } \n"
+                + "                xhttp.open(\"GET\", \"vec?x=\"+nameVar1+\"&y=\"+nameVar2);\n"
                 + "                xhttp.send();\n"
                 + "            }\n"
                 + "      </script>\n"
@@ -94,6 +137,5 @@ public class SparkWebServer {
                 + "</html>\n";
         return response;
     }
-
 
 }
